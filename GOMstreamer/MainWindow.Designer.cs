72f338607f5a0e8d942f6835476d99d287f0e34d
Simon Potter <simon@sjp.co.nz>
2011-01-25 16:29:59 +1300

    First commit. Should work for GSL4.

---
+++ GOMstreamer/MainWindow.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace GOMstreamer
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblVLCloc = new System.Windows.Forms.Label();
+            this.txtVLCloc = new System.Windows.Forms.TextBox();
+            this.btnVLCLoc = new System.Windows.Forms.Button();
+            this.lblStream = new System.Windows.Forms.Label();
+            this.txtdumploc = new System.Windows.Forms.TextBox();
+            this.btnDumpLoc = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblStreamURL = new System.Windows.Forms.Label();
+            this.txtStreamURL = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(12, 26);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(180, 20);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(9, 10);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(73, 13);
+            this.lblEmail.TabIndex = 1;
+            this.lblEmail.Text = "Email Address";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(195, 9);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(198, 26);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(179, 20);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblVLCloc
+            // 
+            this.lblVLCloc.AutoSize = true;
+            this.lblVLCloc.Location = new System.Drawing.Point(9, 62);
+            this.lblVLCloc.Name = "lblVLCloc";
+            this.lblVLCloc.Size = new System.Drawing.Size(71, 13);
+            this.lblVLCloc.TabIndex = 4;
+            this.lblVLCloc.Text = "VLC Location";
+            // 
+            // txtVLCloc
+            // 
+            this.txtVLCloc.Location = new System.Drawing.Point(12, 78);
+            this.txtVLCloc.Name = "txtVLCloc";
+            this.txtVLCloc.Size = new System.Drawing.Size(286, 20);
+            this.txtVLCloc.TabIndex = 2;
+            this.txtVLCloc.Text = "C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe";
+            // 
+            // btnVLCLoc
+            // 
+            this.btnVLCLoc.Location = new System.Drawing.Point(302, 76);
+            this.btnVLCLoc.Name = "btnVLCLoc";
+            this.btnVLCLoc.Size = new System.Drawing.Size(75, 23);
+            this.btnVLCLoc.TabIndex = 3;
+            this.btnVLCLoc.Text = "Open";
+            this.btnVLCLoc.UseVisualStyleBackColor = true;
+            this.btnVLCLoc.Click += new System.EventHandler(this.btnVLCLoc_Click);
+            // 
+            // lblStream
+            // 
+            this.lblStream.AutoSize = true;
+            this.lblStream.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.lblStream.Location = new System.Drawing.Point(9, 115);
+            this.lblStream.Name = "lblStream";
+            this.lblStream.Size = new System.Drawing.Size(128, 13);
+            this.lblStream.TabIndex = 7;
+            this.lblStream.Text = "File to dump the stream to";
+            // 
+            // txtdumploc
+            // 
+            this.txtdumploc.Location = new System.Drawing.Point(12, 131);
+            this.txtdumploc.Name = "txtdumploc";
+            this.txtdumploc.Size = new System.Drawing.Size(286, 20);
+            this.txtdumploc.TabIndex = 4;
+            // 
+            // btnDumpLoc
+            // 
+            this.btnDumpLoc.Location = new System.Drawing.Point(302, 128);
+            this.btnDumpLoc.Name = "btnDumpLoc";
+            this.btnDumpLoc.Size = new System.Drawing.Size(75, 23);
+            this.btnDumpLoc.TabIndex = 5;
+            this.btnDumpLoc.Text = "Open";
+            this.btnDumpLoc.UseVisualStyleBackColor = true;
+            this.btnDumpLoc.Click += new System.EventHandler(this.btnDumpLoc_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(198, 223);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(179, 50);
+            this.btnPlay.TabIndex = 7;
+            this.btnPlay.Text = "Play stream";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 223);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(180, 50);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save stream";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblStreamURL
+            // 
+            this.lblStreamURL.AutoSize = true;
+            this.lblStreamURL.Location = new System.Drawing.Point(12, 172);
+            this.lblStreamURL.Name = "lblStreamURL";
+            this.lblStreamURL.Size = new System.Drawing.Size(65, 13);
+            this.lblStreamURL.TabIndex = 13;
+            this.lblStreamURL.Text = "Stream URL";
+            // 
+            // txtStreamURL
+            // 
+            this.txtStreamURL.Location = new System.Drawing.Point(15, 188);
+            this.txtStreamURL.Name = "txtStreamURL";
+            this.txtStreamURL.Size = new System.Drawing.Size(362, 20);
+            this.txtStreamURL.TabIndex = 8;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 285);
+            this.Controls.Add(this.txtStreamURL);
+            this.Controls.Add(this.lblStreamURL);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnDumpLoc);
+            this.Controls.Add(this.txtdumploc);
+            this.Controls.Add(this.lblStream);
+            this.Controls.Add(this.btnVLCLoc);
+            this.Controls.Add(this.txtVLCloc);
+            this.Controls.Add(this.lblVLCloc);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtEmail);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "GOMstreamer";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblVLCloc;
+        private System.Windows.Forms.TextBox txtVLCloc;
+        private System.Windows.Forms.Button btnVLCLoc;
+        private System.Windows.Forms.Label lblStream;
+        private System.Windows.Forms.TextBox txtdumploc;
+        private System.Windows.Forms.Button btnDumpLoc;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblStreamURL;
+        private System.Windows.Forms.TextBox txtStreamURL;
+    }
+}
+
---
+++ GOMstreamer/MainWindow.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Windows.Forms;
+
+namespace GOMstreamer
+{
+    public partial class MainWindow : Form
+    {
+        string email = "";
+        string pass = "";
+        string vlcloc = "";
+        string dumploc = "";
+        string streamloc = "";
+        bool hasSignedIn = false;
+        CookieContainer cookieJar = new CookieContainer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            vlcloc = txtVLCloc.Text;
+        }
+        
+        private void btnDumpLoc_Click(object sender, EventArgs e)
+        {
+            // Ensure that the saved file is an OGM file
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Ogg Media (*.ogm)|*.ogm";
+            sfd.Title = "Stream dump location";
+
+            DialogResult clickedOK = sfd.ShowDialog();
+
+            // Only assign class variables if the dialog was successful
+            if (clickedOK == DialogResult.OK)
+            {
+                dumploc = sfd.FileName;
+                txtdumploc.Text = dumploc;
+            }
+        }
+
+        private void btnVLCLoc_Click(object sender, EventArgs e)
+        {
+            // Ensure that only VLC is found when the user searches for an executable
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "VLC Executable|vlc.exe";
+            ofd.Title = "VLC Location";
+
+            DialogResult clickedOK = ofd.ShowDialog();
+
+            // Only assign class variables if the dialog was successful
+            if (clickedOK == DialogResult.OK)
+            {
+                vlcloc = ofd.FileName;
+                txtVLCloc.Text = vlcloc;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            email = txtEmail.Text;
+            pass = txtPassword.Text;
+            vlcloc = txtVLCloc.Text;
+            dumploc = txtdumploc.Text;
+
+            // Catch any exceptions and display the message if they're encountered.
+            try
+            {
+                saveStream();
+            }
+            catch (WebException we)
+            {
+                MessageBox.Show(we.Message, "GOMstreamer error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            email = txtEmail.Text;
+            pass = txtPassword.Text;
+            vlcloc = txtVLCloc.Text;
+
+            // Catch any exceptions and display the message if they're encountered.
+            try
+            {
+                playStream();
+            }
+            catch (WebException we)
+            {
+                MessageBox.Show(we.Message, "GOMstreamer error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        private void saveStream()
+        {
+            dumploc = txtdumploc.Text;
+            string streamURL = "";
+
+            // If we haven't signed in yet, then we haven't got the stream URL.
+            // Otherwise we've got cookies and hopefully a correct stream, meaning we can skip
+            // having to get the stream URL again.
+            if (!hasSignedIn)
+            {
+                streamURL = getStreamURL();
+            }
+            else
+            {
+                streamURL = streamloc;
+            }
+
+            // If the user has changed the text field, update it with the correct value
+            if (txtStreamURL.Text != streamURL)
+            {
+                txtStreamURL.Text = streamURL;
+            }
+
+            // Run VLC with the correct arguments
+            string vlcargs = "--http-caching=30000 " + streamURL +
+                             " :demux=dump :demuxdump-file=\"" + dumploc + "\"" +
+                             " vlc://quit";
+            Process vlc = new Process();
+            vlc.StartInfo.UseShellExecute = true;
+            vlc.StartInfo.FileName = vlcloc;
+            vlc.StartInfo.Arguments = vlcargs;
+            vlc.Start();
+        }
+
+        private void playStream()
+        {
+            string streamURL = "";
+
+            // If we haven't signed in yet, then we haven't got the stream URL.
+            // Otherwise we've got cookies and hopefully a correct stream, meaning we can skip
+            // having to get the stream URL again.
+            if (!hasSignedIn)
+            {
+                streamURL = getStreamURL();
+            }
+            else
+            {
+                streamURL = streamloc;
+            }
+
+            // If the user has changed the text field, update it with the correct value
+            if (txtStreamURL.Text != streamURL)
+            {
+                txtStreamURL.Text = streamURL;
+
+            }
+
+            // Run VLC with the correct arguments
+            string vlcargs = "--http-caching=30000 " + streamURL +
+                             " vlc://quit";
+            Process vlc = new Process();
+            vlc.StartInfo.UseShellExecute = true;
+            vlc.StartInfo.FileName = vlcloc;
+            vlc.StartInfo.Arguments = vlcargs;
+            vlc.Start();
+        }
+        
+        private string getStreamURL()
+        {
+            string gomtvURL = "http://www.gomtv.net";
+            string gomtvLiveURL = gomtvURL + "/2011gslsponsors1/live/";
+            
+            // Signing into GOMtv
+            if (!hasSignedIn)
+            {
+                cookieJar = new CookieContainer();
+                signIn();
+            }
+
+            // Now that we have the cookies that we need to authenticate further interaction
+            // we should be able to load the Live page and grab the stream from there.
+            HttpWebRequest liveReq = (HttpWebRequest)WebRequest.Create(gomtvLiveURL);
+            liveReq.Method = "GET";
+            liveReq.CookieContainer = cookieJar;
+            liveReq.Timeout = 15000;  // Ensuring no long wait if the webserver is down
+            HttpWebResponse liveResponse = (HttpWebResponse)liveReq.GetResponse();  // Grabbing live page
+            StreamReader reader = new StreamReader(liveResponse.GetResponseStream(), Encoding.UTF8);
+            string liveHTML = reader.ReadToEnd();
+            reader.Close();
+            reader.Dispose();
+            liveResponse.Close();
+
+            string goxurl = parseGOXUrl(liveHTML);  // Getting the GOX XML file
+            string httpStream = parseHTTPStream(goxurl);  // Getting the HTTP stream from the GOX XML file
+            streamloc = httpStream;
+            return httpStream;
+        }
+
+        private void signIn()
+        {
+            string gomtvURL = "http://www.gomtv.net";
+            string gomtvSignInURL = gomtvURL + "/user/loginProcess.gom";
+            string httpEmail = HttpUtility.UrlEncode(email);  // The email & password will have special characters that need decoding
+            string httpPass = HttpUtility.UrlEncode(pass);
+            string postdata = "rememberme=1&cmd=login&mb_username=" + httpEmail + "&mb_password=" + httpPass;  // Forming post data string
+            byte[] byteArray = Encoding.UTF8.GetBytes(postdata);  // Send data with UTF-8 encoding
+            
+            // Signing into GOMtv
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(gomtvSignInURL);
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.ContentLength = byteArray.Length;
+            req.CookieContainer = cookieJar;
+
+            // Sending login data to the correct URL
+            Stream dataStream = req.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            dataStream.Dispose();
+            HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+            response.Close();
+
+            // Checking to see for a failed sign-in
+            if (cookieJar.Count == 0)
+                throw new WebException("Authentication failed. Please check your username and password.");
+
+            // We have signed in correctly and received correct cookies
+            hasSignedIn = true;
+        }
+
+        private string parseGOXUrl(string liveHTML)
+        {
+            // The live page has been collected. Now to parse for the stream via regex
+            Regex linkRegex = new Regex("http://www.gomtv.net/gox[^;]+;");
+            Match m = linkRegex.Match(liveHTML);
+            string urlFromHTML = "";
+
+            if (m.Success)
+            {
+                urlFromHTML = m.Groups[0].Value;
+            }
+            else
+            {
+                throw new WebException("Unable to parse GOX XML URL from the live page.");
+            }
+
+            urlFromHTML = urlFromHTML.Replace("\" + playType + \"", "SQTest");
+            urlFromHTML = Regex.Replace(urlFromHTML, "\"[^;]+;", "");
+
+            Regex titleRegex = new Regex("this.title[^;]+;");
+            m = titleRegex.Match(liveHTML);
+            string titleFromHTML = "";
+
+            if (m.Success)
+            {
+                titleFromHTML = m.Groups[0].Value;
+            }
+            else
+            {
+                throw new WebException("Unable to parse stream title from the live page.");
+            }
+
+            titleRegex = new Regex("\"(.*)\"");
+            m = titleRegex.Match(titleFromHTML);
+
+            if (m.Success)
+            {
+                titleFromHTML = m.Groups[0].Value;
+            }
+            else
+            {
+                throw new WebException("Unable to parse stream title from the live page.");
+            }
+
+            titleFromHTML = titleFromHTML.Replace("\"", "");
+            string goxurl = urlFromHTML + titleFromHTML;
+            return goxurl;
+        }
+
+        private string parseHTTPStream(string goxurl)
+        {
+            // Now we have the link to the XML file that contains the actual stream link
+            // Grabbing it now
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(goxurl);
+            req.Method = "GET";
+            req.CookieContainer = cookieJar;  // Using the authenticated cookies
+            HttpWebResponse response = (HttpWebResponse)req.GetResponse();  // Grabbing the GOX XML file
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            string GOXxml = reader.ReadToEnd();
+            reader.Close();
+            reader.Dispose();
+            response.Close();
+
+            // Have the XML file, now to parse for the stream link
+            Regex goxRegex = new Regex("<REF href=\"([^\"]*)\"/>");
+            Match m = goxRegex.Match(GOXxml);
+            string streamURL = "";
+
+            if (m.Success)
+            {
+                streamURL = m.Groups[1].Value;
+            }
+            else
+            {
+                throw new WebException("Unable to parse gomcmd URL from the GOX XML file.");
+            }
+
+            goxRegex = new Regex("(http%3[Aa].+)&quot;");
+            m = goxRegex.Match(GOXxml);
+
+            if (m.Success)
+            {
+                streamURL = m.Groups[0].Value;
+            }
+            else
+            {
+                throw new WebException("Unable to parse HTTP stream from gomcmd URL.");
+            }
+
+            streamURL = HttpUtility.UrlDecode(streamURL); // Creating a more readable stream URL
+            streamURL = streamURL.Replace("&amp;", "&");  // Decoding &amp; HTML entity
+            streamURL = streamURL.Replace("&quot;", "");  // Removing an unnecessary HTML entity
+            return streamURL;
+        }
+    }
+}
